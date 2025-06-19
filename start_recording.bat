bb0a6acdf917718da0c776413b2dd5f97af7aa7e
seiji6wt <seiji6wt@gmail.com>
2025-06-19 21:56:50 +0900

    ignore dialog

--- start_recording.bat
+++ start_recording.bat
@@ -1,6 +1,6 @@
 @echo off
 cd /d "C:\Program Files\obs-studio\bin\64bit"
-start /b "" "obs64.exe" --startrecording --minimize-to-tray
+start /b "" "obs64.exe" --disable-shutdown-check --startrecording --minimize-to-tray
 
 
 
--- stop_recording.bat
+++ stop_recording.bat
@@ -1,5 +1,2 @@
 @echo off
-cd "C:\Program Files\obs-studio\bin\64bit"
-"obs64.exe" --stoprecording
-timeout /t 3
 taskkill /IM obs64.exe /F /T
